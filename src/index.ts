91a68410657097a7aef49df88eedb0f5ba66e2cb
WMXPY <wm@mengw.io>
2019-05-04 22:11:53 -0500

    remove red routes

--- src/index.ts
+++ src/index.ts
@@ -11,22 +11,6 @@ import * as Path from 'path';
 import { ApplicationRoute } from './routes/portal/application';
 import { RetrieveRoute } from './routes/portal/retrieve';
 import { AccountValidateRoute } from './routes/portal/validate';
-import { AddGroupRoute } from './routes/red/account/add-group';
-import { AdminEditRoute } from './routes/red/account/admin-edit';
-import { ChangePasswordRoute } from './routes/red/account/change-password';
-import { FetchAccountRoute } from './routes/red/account/fetch';
-import { RegisterRoute } from './routes/red/account/register';
-import { SelfEditRoute } from './routes/red/account/self-edit';
-import { SingleAccountRoute } from './routes/red/account/single';
-import { CreateApplicationRoute } from './routes/red/application/create';
-import { FetchApplicationRoute } from './routes/red/application/fetch';
-import { SingleApplicationRoute } from './routes/red/application/single';
-import { UpdateApplicationRoute } from './routes/red/application/update';
-import { CreateGroupRoute } from './routes/red/group/create';
-import { FetchGroupRoute } from './routes/red/group/fetch';
-import { GlobalPreferenceRoute } from './routes/red/preference/global';
-import { InfosPreferenceRoute } from './routes/red/preference/infos';
-import { ReadPreferenceRoute } from './routes/red/preference/read';
 import { BrontosaurusConfig, isDevelopment, readConfigEnvironment } from './util/conf';
 import { registerConnor } from './util/error';
 
@@ -63,34 +47,6 @@ app.routes(
     new RetrieveRoute(),
     new ApplicationRoute(),
     new AccountValidateRoute(),
-);
-
-// Red
-app.routes(
-
-    // Application
-    new CreateApplicationRoute(),
-    new FetchApplicationRoute(),
-    new SingleApplicationRoute(),
-    new UpdateApplicationRoute(),
-
-    // Group
-    new CreateGroupRoute(),
-    new FetchGroupRoute(),
-
-    // Account
-    new AddGroupRoute(),
-    new FetchAccountRoute(),
-    new RegisterRoute(),
-    new SelfEditRoute(),
-    new ChangePasswordRoute(),
-    new AdminEditRoute(),
-    new SingleAccountRoute(),
-
-    // Preference
-    new GlobalPreferenceRoute(),
-    new ReadPreferenceRoute(),
-    new InfosPreferenceRoute(),
 );
 
 app.host(8080);
