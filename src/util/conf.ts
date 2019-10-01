3c264bbc6964c3bca5b80a844b2a42e46832d26e
WMXPY <wm@mengw.io>
2019-10-01 13:11:59 -0500

    remvoe config, add email to token

--- src/util/conf.ts
+++ src/util/conf.ts
@@ -4,27 +4,9 @@
  * @description Conf
  */
 
-import * as Fs from 'fs';
-import * as Path from 'path';
-
 export type BrontosaurusConfig = {
 
     database: string;
-};
-
-export const readConfigSync = (): BrontosaurusConfig => {
-
-    try {
-
-        const configPath: string = Path.join(__dirname, '..', '..', 'brontosaurus.conf');
-        return JSON.parse(Fs.readFileSync(configPath, 'utf8'));
-    } catch (err) {
-
-        console.log('Config file not exist');
-        process.exit();
-    }
-
-    throw new Error('never');
 };
 
 export const readConfigEnvironment = (): BrontosaurusConfig => {
