53fa3effe12d24b9bc4fb0bb155a19a11bc90b16
WMXPY <wm@mengw.io>
2020-03-13 14:34:12 -0500

    fix validate route

--- src/routes/portal/validate.ts
+++ src/routes/portal/validate.ts
@@ -5,7 +5,7 @@
  */
 
 import { BrontosaurusToken } from "@brontosaurus/core";
-import { AccountController, ApplicationController, IAccountModel, IApplicationModel, INamespaceModel, NamespaceController } from "@brontosaurus/db";
+import { ApplicationController, IAccountModel, IApplicationModel, MatchController } from "@brontosaurus/db";
 import { IBrontosaurusBody } from "@brontosaurus/definition";
 import { ROUTE_MODE, SudooExpressHandler, SudooExpressNextFunction, SudooExpressRequest, SudooExpressResponse } from "@sudoo/express";
 import { Safe, SafeExtract } from '@sudoo/extract';
@@ -48,19 +48,11 @@ export class AccountValidateRoute extends BrontosaurusRoute {
                 private: application.privateKey,
             }, application.expire, token);
 
-            const account: IAccountModel | null = await AccountController.getAccountByUsername(brontosaurus.username);
-            const namespace: INamespaceModel | null = await NamespaceController.getNamespaceByNamespace(brontosaurus.namespace);
+            const account: IAccountModel | null = await MatchController.getAccountByUsernameAndNamespaceName(brontosaurus.username, brontosaurus.namespace);
 
             if (!account) {
+
                 throw this._error(ERROR_CODE.ACCOUNT_NOT_FOUND, brontosaurus.username);
-            }
-
-            if (!namespace) {
-                throw this._error(ERROR_CODE.NAMESPACE_NOT_FOUND, brontosaurus.namespace);
-            }
-
-            if (account.namespace.toHexString() !== namespace._id.toString()) {
-                throw this._error(ERROR_CODE.ACCOUNT_NAMESPACE_NOT_MATCH, account.username, namespace.namespace);
             }
 
             if (!account.active) {
