ff5758aed56e984666aae28e5b5c6d47a1d0704a
WMXPY <wm@mengw.io>
2020-10-05 12:51:03 -0500

    add email unit test more

--- src/util/email.ts
+++ src/util/email.ts
@@ -11,18 +11,33 @@ export const compareEmail = (left?: string, right?: string): boolean => {
         return false;
     }
 
-    const leftSplited: string[] = left.split('+');
-    const rightSplited: string[] = right.split('+');
+    const leftDomainSplited: string[] = left.split('@');
+    const rightDomainSplited: string[] = right.split('@');
 
-    if (leftSplited.length === 1
-        && rightSplited.length === 1) {
-        return left.toLowerCase() === right.toLowerCase();
-    }
-
-    if (typeof leftSplited[0] !== 'string'
-        || typeof rightSplited[0] !== 'string') {
+    if (leftDomainSplited.length !== 2
+        || rightDomainSplited.length !== 2) {
         return false;
     }
 
-    return leftSplited[0].toLowerCase() === rightSplited[0].toLowerCase();
+    const leftPLusSplited: string[] = left.split('+');
+    const rightPLusSplited: string[] = right.split('+');
+
+    if (leftPLusSplited.length === 1
+        && rightPLusSplited.length === 1) {
+        return left.toLowerCase() === right.toLowerCase();
+    }
+
+    if (typeof leftPLusSplited[0] !== 'string'
+        || typeof rightPLusSplited[0] !== 'string') {
+        return false;
+    }
+
+    const leftRejoin: string = leftPLusSplited.length === 1
+        ? left
+        : leftPLusSplited[0] + '@' + leftDomainSplited[1];
+    const rightRejoin: string = rightPLusSplited.length === 1
+        ? right
+        : rightPLusSplited[0] + '@' + rightDomainSplited[1];
+
+    return leftRejoin.toLowerCase() === rightRejoin.toLowerCase();
 };
