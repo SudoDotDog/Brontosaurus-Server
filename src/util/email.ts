5ecfc1d85f0713eeaaeb744364b4efd6bd4bb619
WMXPY <wm@mengw.io>
2020-10-05 12:43:33 -0500

    improve email validate

--- src/util/email.ts
+++ src/util/email.ts
@@ -6,9 +6,23 @@
 
 export const compareEmail = (left?: string, right?: string): boolean => {
 
-    if (typeof left !== 'string' || typeof right !== 'string') {
+    if (typeof left !== 'string'
+        || typeof right !== 'string') {
         return false;
     }
 
-    return left.toLowerCase() === right.toLowerCase();
+    const leftSplited: string[] = left.split('+');
+    const rightSplited: string[] = right.split('+');
+
+    if (leftSplited.length === 1
+        && rightSplited.length === 1) {
+        return left.toLowerCase() === right.toLowerCase();
+    }
+
+    if (typeof leftSplited[0] !== 'string'
+        || typeof rightSplited[0] !== 'string') {
+        return false;
+    }
+
+    return leftSplited[0].toLowerCase() === rightSplited[0].toLowerCase();
 };
